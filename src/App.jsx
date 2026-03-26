b186c0578b45a219bf46e9e9cb90adf67c9b3181
Patrick <johnpatrickdelacruz40@gmail.com>
2026-03-27 03:09:35 +0800

    Added Admin route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ScrollHandler from './components/ScrollHandler'; // <-- ADD THIS
 import Layout from './components/Layout';
+import ScrollHandler from './components/ScrollHandler';
+
+// Page Imports
 import Home from './pages/Home';
+import About from './pages/About';
 import Academics from './pages/Academics';
 import People from './pages/People';
-import About from './pages/About'; 
-import Experience from './pages/Experience'; 
-import Media from './pages/Media'; 
+import Experience from './pages/Experience';
+import Media from './pages/Media';
+import Admin from './pages/Admin'; // <-- Your new Admin page!
 
 export default function App() {
   return (
     <Router>
-      <ScrollHandler /> {/* <-- ADD THIS HERE */}
+      <ScrollHandler />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} /> 
-          <Route path="people" element={<People />} />
+          <Route path="about" element={<About />} />
           <Route path="academics" element={<Academics />} />
-          <Route path="experience" element={<Experience />} /> 
+          <Route path="people" element={<People />} />
+          <Route path="experience" element={<Experience />} />
           <Route path="media" element={<Media />} />
+          {/* THE NEW ADMIN ROUTE */}
+          <Route path="admin" element={<Admin />} />
         </Route>
       </Routes>
     </Router>
